5a8058d8c579fe17a3fb63c8fcde5c8e9e74e748
JAHONGIR SAIDIY <jahongirsaidiy@gmail.com>
2019-05-27 11:40:46 +0500

    Created all files

---
+++ goodbye.php
@@ -0,0 +1,4 @@
+<?php
+	echo "Goodbye";
+	echo "Finish";
+?>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php 
+
+echo "Hello world!;";
+echo "What`s up?!;";
+echo "Hello my dear friend;";
+echo "What are you doing?!;";
+
+?>
